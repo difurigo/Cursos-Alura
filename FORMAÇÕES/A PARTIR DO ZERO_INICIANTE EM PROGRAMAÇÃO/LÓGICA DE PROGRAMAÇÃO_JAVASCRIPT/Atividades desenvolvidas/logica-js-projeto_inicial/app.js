67775b27d5d23775f3d7e17edac088f7202e42f4
difurigo <furigo9diego@gmail.com>
2024-05-14 00:03:37 -0300

    Capitulo 2

--- FORMAÇÕES/A PARTIR DO ZERO_INICIANTE EM PROGRAMAÇÃO/LÓGICA DE PROGRAMAÇÃO_JAVASCRIPT/Atividades desenvolvidas/logica-js-projeto_inicial/app.js
+++ FORMAÇÕES/A PARTIR DO ZERO_INICIANTE EM PROGRAMAÇÃO/LÓGICA DE PROGRAMAÇÃO_JAVASCRIPT/Atividades desenvolvidas/logica-js-projeto_inicial/app.js
@@ -1,15 +1,17 @@
 // Código aprendido na aula (Fiz algumas coisas a mais, pois estava muito simples)
 alert("Boas vindas ao jogo do número secreto!");
 let numeroSecreto = 11;
+console.log(numeroSecreto);
 let chute = prompt("Digite um número entre 1 e 20:");
 
 if (chute == numeroSecreto) {
-    console.log("Você acertou! O número era 11");
+    alert(`Você acertou! O número era ${numeroSecreto}!`);
 } else {
     if (chute > numeroSecreto) {
+        // Template String:
         alert("Você errou. O número que pensei é menor.");
     } else {
         alert("Você errou. O número que pensei é maior.");
     }
     window.location.reload();
-}
+} 
