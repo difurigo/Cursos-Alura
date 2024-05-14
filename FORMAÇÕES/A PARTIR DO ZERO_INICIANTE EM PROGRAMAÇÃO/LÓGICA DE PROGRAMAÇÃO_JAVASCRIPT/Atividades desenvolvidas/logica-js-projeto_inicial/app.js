dd060299917a734ca14a906c3422133002d9c600
difurigo <furigo9diego@gmail.com>
2024-05-14 15:09:51 -0300

    aula 4

--- FORMAÇÕES/A PARTIR DO ZERO_INICIANTE EM PROGRAMAÇÃO/LÓGICA DE PROGRAMAÇÃO_JAVASCRIPT/Atividades desenvolvidas/logica-js-projeto_inicial/app.js
+++ FORMAÇÕES/A PARTIR DO ZERO_INICIANTE EM PROGRAMAÇÃO/LÓGICA DE PROGRAMAÇÃO_JAVASCRIPT/Atividades desenvolvidas/logica-js-projeto_inicial/app.js
@@ -1,15 +1,15 @@
 // Código aprendido na aula (Fiz algumas coisas a mais, pois estava muito simples)
 alert("Boas vindas ao jogo do número secreto!");
-let numeroSecreto = 11;
+let multiplicador = 20;
+let numeroSecreto = parseInt((Math.random() * multiplicador) + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 while (chute != numeroSecreto) {
-    chute = prompt("Digite um número entre 1 e 20:");
+    chute = prompt(`Digite um número entre 1 e ${multiplicador}:`);
     if (chute == numeroSecreto) {
-        // Template String:
-        alert(`Você acertou! O número era ${numeroSecreto}! Foram ${tentativas} tentativas.`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert("Você errou. O número que pensei é menor.");
@@ -19,3 +19,13 @@ while (chute != numeroSecreto) {
         tentativas++;
     } 
 }
+
+let variacaoTentativa = tentativas > 1 ? "tentativas." : "tentativa";
+alert(`Você acertou! O número era ${numeroSecreto}! Em ${tentativas} ${variacaoTentativa}`);
+
+// if (tentativas > 1) {
+//     // Template String:
+//     alert(`Você acertou! O número era ${numeroSecreto}! Foram ${tentativas} tentativas.`);
+// } else {
+//     alert(`Caramba você acertou de primeira! O número era ${numeroSecreto} mesmo!`);
+// }
