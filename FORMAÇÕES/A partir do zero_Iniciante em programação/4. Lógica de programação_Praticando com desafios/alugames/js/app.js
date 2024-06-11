65e560d2e1c66f0e82fdcc7d7a61dbc4b67b40e5
difurigo <furigo9diego@gmail.com>
2024-06-11 15:05:26 -0300

    Melhorias no projeto Alugames

--- FORMAÇÕES/A partir do zero_Iniciante em programação/4. Lógica de programação_Praticando com desafios/alugames/js/app.js
+++ FORMAÇÕES/A partir do zero_Iniciante em programação/4. Lógica de programação_Praticando com desafios/alugames/js/app.js
@@ -1,14 +1,28 @@
+let jogosAlugados = 0;
+function exibirQuantosJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
 function alterarStatus(numero) {
     let propriedadeImg = document.getElementById(`game-${numero}`).querySelector("div");
     let propriedadeBtn = document.getElementById(`game-${numero}`).querySelector("a");
+    let propriedadeNome = document.getElementById(`game-${numero}`).querySelector("p");
     
     if (propriedadeImg.classList.contains("dashboard__item__img--rented")) {
-        propriedadeImg.classList.remove("dashboard__item__img--rented");
-        propriedadeBtn.classList.remove("dashboard__item__button--return");
-        propriedadeBtn.innerText = "Alugar";
+
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${propriedadeNome.textContent}?`)) {
+            propriedadeImg.classList.remove("dashboard__item__img--rented");
+            propriedadeBtn.classList.remove("dashboard__item__button--return");
+            propriedadeBtn.innerText = "Alugar";
+            jogosAlugados--;
+        }
+
     } else {
         propriedadeImg.classList.add("dashboard__item__img--rented");
         propriedadeBtn.classList.add("dashboard__item__button--return");
         propriedadeBtn.innerText = "Devolver";
+        jogosAlugados++;
     }
+
+    exibirQuantosJogosAlugados();
 }
