244e4d47e24597596f2dd15b8a50586dea5e54b8
difurigo <furigo9diego@gmail.com>
2024-06-12 19:46:05 -0300

    Melhorias do amigo-secreto

--- FORMAÇÕES/A partir do zero_Iniciante em programação/4. Lógica de programação_Praticando com desafios/amigo-secreto/js/app.js
+++ FORMAÇÕES/A partir do zero_Iniciante em programação/4. Lógica de programação_Praticando com desafios/amigo-secreto/js/app.js
@@ -24,6 +24,9 @@ function adicionar() {
 
   amigo.value = "";
   amigo.focus();
+
+  atualizarLista();
+  atualizarSorteio();
 }
 
 function sortear() {
@@ -45,6 +48,12 @@ function sortear() {
   }
 }
 
+function excluirAmigo(index) {
+  amigos.splice(index, 1);
+  atualizarLista();
+  atualizarSorteio();
+}
+
 function embaralhar(lista) {
   for (let indice = lista.length; indice; indice--) {
     const indiceAleatorio = Math.floor(Math.random() * indice);
@@ -52,6 +61,31 @@ function embaralhar(lista) {
       lista[indiceAleatorio],
       lista[indice - 1],
     ];
+  }
+}
+
+function atualizarSorteio() {
+  let sorteio = document.getElementById("lista-sorteio");
+  sorteio.innerHTML = "";
+}
+
+function atualizarLista() {
+  let lista = document.getElementById("lista-amigos");
+  lista.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    // Cria um elemento de parágrafo para cada amigo
+    let paragrafo = document.createElement("p");
+    paragrafo.textContent = amigos[i];
+
+    // Adiciona um evento de clique para excluir o amigo
+    paragrafo.addEventListener("click", function () {
+      excluirAmigo(i);
+    });
+
+    // Adiciona o parágrafo à lista
+    lista.appendChild(paragrafo);
+    paragrafo.classList.add("remove-amigo");
   }
 }
 
