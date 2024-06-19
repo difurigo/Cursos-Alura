14c9cc718bf639d249d91db52fd4c3f7445508a3
difurigo <furigo9diego@gmail.com>
2024-06-19 15:28:56 -0300

    Modulo 5 do curso de python 2 completo

--- FORMAÇÕES/Aprenda a programar em Python com Orientação a Objetos/2. Python_aplicando a Orientação a Objetos/oo-sabor-express/app.py
+++ FORMAÇÕES/Aprenda a programar em Python com Orientação a Objetos/2. Python_aplicando a Orientação a Objetos/oo-sabor-express/app.py
@@ -1,17 +1,19 @@
 from modelos.restaurante import Restaurante
+import os
 
 restaurante_burguer_king = Restaurante(nome='Burguer King', categoria='Fast-Food')
-restaurante_burguer_king.receber_avaliacao(cliente='Diego', nota=9)
-restaurante_burguer_king.receber_avaliacao(cliente='Vanessa', nota=8)
+restaurante_burguer_king.receber_avaliacao(cliente='Diego', nota=4)
+restaurante_burguer_king.receber_avaliacao(cliente='Vanessa', nota=3.6)
 
 restaurante_badaroscas = Restaurante(nome='Badaroscas', categoria='Porção')
 restaurante_badaroscas.alternar_status()
 restaurante_badaroscas.receber_avaliacao(cliente='Nicolle', nota=3)
-restaurante_badaroscas.receber_avaliacao(cliente='Diego', nota=9.3)
+restaurante_badaroscas.receber_avaliacao(cliente='Diego', nota=4.3)
 
 restaurante_do_son = Restaurante(nome='Restaurante do Son', categoria='Self-Service')
 
 def main():
+    os.system('cls')
     Restaurante.listar_restaurantes()
 
 if __name__ == '__main__':
--- FORMAÇÕES/Aprenda a programar em Python com Orientação a Objetos/2. Python_aplicando a Orientação a Objetos/oo-sabor-express/modelos/restaurante.py
+++ FORMAÇÕES/Aprenda a programar em Python com Orientação a Objetos/2. Python_aplicando a Orientação a Objetos/oo-sabor-express/modelos/restaurante.py
@@ -1,9 +1,18 @@
 from modelos.avaliacao import Avaliacao
 
 class Restaurante():
+    '''Representa um restaurante e suas características.'''
+
     restaurantes = []
 
     def __init__(self, nome: str, categoria: str):
+        '''
+        Inicializa uma instância de Restaurante.
+
+        Parâmetros:
+        - nome (str): O nome do restaurante.
+        - categoria (str): A categoria do restaurante.
+        '''
         self._nome = nome.title()
         self._categoria = categoria
         self._ativo = False
@@ -11,10 +20,12 @@ class Restaurante():
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
+        '''Retorna uma representação em string do restaurante.'''
         return f'Nome: {self.nome} | Categoria: {self.categoria}'
 
     @classmethod
     def listar_restaurantes(cls):
+        '''Exibe uma lista formatada de todos os restaurantes.'''
         print('-' * 76)
         print(f'{'    Nome do restaurante'.ljust(26)} | {'      Categoria'.ljust(21)} | {' Avaliação'.ljust(11)} |  Status')
         print('-' * 76)
@@ -26,27 +37,40 @@ class Restaurante():
 
     @property
     def ativo(self):
+        '''Retorna um símbolo indicando o estado de atividade do restaurante.'''
         return '✅' if self._ativo else '❎'
     
     @property
     def nome(self):
+        '''Retorna o nome que foi atribuído ao restaurante'''
         return self._nome
     
     @property
     def categoria(self):
+        '''Retorna a categoria que foi atribuída ao restaurante'''
         return self._categoria
     
     def alternar_status(self):
+        '''Alterna o status de atividade do restaurante.'''
         self._ativo = not self._ativo
 
     def receber_avaliacao(self, cliente, nota: float):
-        avaliacao = Avaliacao(cliente=cliente, nota=nota)
-        self._avaliacao.append(avaliacao)
+        '''
+        Registra uma avaliação para o restaurante.
+
+        Parâmetros:
+        - cliente (str): O nome do cliente que fez a avaliação.
+        - nota (float): A nota atribuída ao restaurante (entre 1 e 5).
+        '''
+        if 0 <= nota <= 5:
+            avaliacao = Avaliacao(cliente=cliente, nota=nota)
+            self._avaliacao.append(avaliacao)
     
     @property
     def media_avaliacoes(self):
+        '''Calcula e retorna a média das avaliações do restaurante.'''
         if not self._avaliacao:
-            return 0
+            return ' -'
         
         soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
         quantidade_de_notas = len(self._avaliacao)
